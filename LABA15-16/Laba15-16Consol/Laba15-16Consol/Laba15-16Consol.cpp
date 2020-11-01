e23447c32b065bb7dcecc8f8696e743ea7a6f145
UstinovMS <73177764+UstinovMS@users.noreply.github.com>
2020-11-01 20:54:54 +0300

    Laba15-16

---
+++ LABA15-16/Laba15-16Consol/Laba15-16Consol/Laba15-16Consol.cpp
@@ -0,0 +1,24 @@
+﻿/*Создать пустой проект Console Application и реализовать в нем программу по
+вычислению стоимости поездки на автомобиле(исходными данными являются : километраж,
+расход топлива, цена единицы топлива).*/
+
+#include "pch.h"
+#include <iostream>
+#include <locale>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	double km, pricegaz, gaz, price;
+	cout << "Введите исходные данные." << endl;
+	cout << "Километраж: "; cin >> km; cout << endl;
+	cout << "Расход топлива на 100 км пути:: "; cin >> gaz; cout << endl;
+	cout << "Стоимость бензина: "; cin >> pricegaz; cout << endl;
+	gaz = gaz / 100;
+	price = gaz * km * pricegaz;
+	cout << "Стоимость поездки: " << price << " руб." << endl << endl;
+	system("pause");
+	                                                                                                        
+}
